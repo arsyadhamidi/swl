827d52e181a8cc8fda69b04fdcd57c6cc9e2d1e7
Arsyad Hamidi <arsyadhamidi04@gmail.com>
2026-06-25 10:57:04 +0700

    commit

--- database/migrations/2026_03_11_142913_create_keranjang_details_table.php
+++ database/migrations/2026_03_11_142913_create_keranjang_details_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('keranjang_details', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->integer('keranjang_id');
-            $table->integer('barang_id');
+            $table->integer('barang_variasi_id');
             $table->integer('jumlah');
             $table->decimal('harga');
             $table->decimal('subtotal');
