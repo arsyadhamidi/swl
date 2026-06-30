b13e91ffba94bdeeb6ad6a45bb731ae12949fc6d
Arsyad Hamidi <arsyadhamidi04@gmail.com>
2026-06-30 14:42:48 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Kategori;
 use App\Models\Ongkir;
 use App\Models\User;
 use Carbon\Carbon;
@@ -26,7 +27,7 @@ class DatabaseSeeder extends Seeder
             'telp' => '628111111111',
             'level_id' => '1',
             'alamat' => '-',
-            'email_verified_at' => $carbons
+            'email_verified_at' => $carbons,
         ]);
 
         $ongkirs = [
@@ -38,12 +39,30 @@ class DatabaseSeeder extends Seeder
             ['kota' => 'Padang Panjang', 'biaya' => 22000],
             ['kota' => 'Sawahlunto',  'biaya' => 25000],
             ['kota' => 'Painan',      'biaya' => 27000],
-            ['kota' => 'Lubuk Basung','biaya' => 30000],
+            ['kota' => 'Lubuk Basung', 'biaya' => 30000],
             ['kota' => 'Pekanbaru',   'biaya' => 50000],
         ];
 
         foreach ($ongkirs as $ongkir) {
             Ongkir::create($ongkir);
+        }
+
+        // Seeder Kategori
+        $kategoris = [
+            ['nm_kategori' => 'Baju Anak Laki-laki'],
+            ['nm_kategori' => 'Baju Anak Perempuan'],
+            ['nm_kategori' => 'Celana Anak'],
+            ['nm_kategori' => 'Rok Anak'],
+            ['nm_kategori' => 'Jaket Anak'],
+            ['nm_kategori' => 'Kaos Anak'],
+            ['nm_kategori' => 'Setelan Anak'],
+            ['nm_kategori' => 'Pakaian Muslim Anak'],
+            ['nm_kategori' => 'Sepatu Anak'],
+            ['nm_kategori' => 'Aksesoris Anak'],
+        ];
+
+        foreach ($kategoris as $kategori) {
+            Kategori::create($kategori);
         }
     }
 }
